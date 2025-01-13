1538d92455105b7b6083491f778d59cddfcfca89
Theo <theo.dev.1234567890@gmail.com>
2025-01-13 10:08:47 -0300

    Update cache_manager.go

--- cache_manager/cache_manager.go
+++ cache_manager/cache_manager.go
@@ -7,87 +7,87 @@ import (
 )
 
 type Instance_cache struct {
-	health int32
-	value  any
+	Health int32
+	Value  any
 }
 
 type Mem_cache struct {
-	cache     map[[32]byte]Instance_cache
-	life_time int32
-	mu        sync.Mutex
+	Cache     map[[32]byte]Instance_cache
+	Life_time int32
+	Mu        sync.Mutex
 }
 
-func (entity *Mem_cache) start() {
-	entity.cache = make(map[[32]byte]Instance_cache)
-	entity.life_time = 54000
+func (entity *Mem_cache) Start() {
+	entity.Cache = make(map[[32]byte]Instance_cache)
+	entity.Life_time = 54000
 
 	go func() {
 		for {
-			entity.mu.Lock()
-			for key, value := range entity.cache {
-				if value.health-1 <= 0 {
-					delete(entity.cache, key)
+			entity.Mu.Lock()
+			for key, value := range entity.Cache {
+				if value.Health-1 <= 0 {
+					delete(entity.Cache, key)
 				} else {
-					value.health -= 1
-					entity.cache[key] = value
+					value.Health -= 1
+					entity.Cache[key] = value
 				}
 			}
-			entity.mu.Unlock()
+			entity.Mu.Unlock()
 			time.Sleep(1 * time.Second)
 		}
 	}()
 }
 
-func (entity *Mem_cache) get_key(key string) [32]byte {
+func (entity *Mem_cache) Get_key(key string) [32]byte {
 	return sha256.Sum256([]byte(key))
 }
 
-func (entity *Mem_cache) set(key string, value any) {
-	address := entity.get_key(key)
+func (entity *Mem_cache) Set(key string, value any) {
+	address := entity.Get_key(key)
 	val := Instance_cache{
-		health: entity.life_time,
-		value:  value,
+		Health: entity.Life_time,
+		Value:  value,
 	}
 
-	entity.mu.Lock()
-	entity.cache[address] = val
-	entity.mu.Unlock()
+	entity.Mu.Lock()
+	entity.Cache[address] = val
+	entity.Mu.Unlock()
 }
 
-func (entity *Mem_cache) remove(key string) {
-	address := entity.get_key(key)
-	entity.kill(address)
+func (entity *Mem_cache) Remove(key string) {
+	address := entity.Get_key(key)
+	entity.Kill(address)
 }
 
-func (entity *Mem_cache) kill(address [32]byte) {
-	entity.mu.Lock()
-	delete(entity.cache, address)
-	entity.mu.Unlock()
+func (entity *Mem_cache) Kill(address [32]byte) {
+	entity.Mu.Lock()
+	delete(entity.Cache, address)
+	entity.Mu.Unlock()
 }
 
-func (entity *Mem_cache) get(key string) Instance_cache {
-	address := entity.get_key(key)
+func (entity *Mem_cache) Get(key string) Instance_cache {
+	address := entity.Get_key(key)
 
-	entity.mu.Lock()
-	defer entity.mu.Unlock()
+	entity.Mu.Lock()
+	defer entity.Mu.Unlock()
 
-	return entity.extract(address)
+	return entity.Extract(address)
 }
 
-func (entity *Mem_cache) extract(address [32]byte) Instance_cache {
-	val, exists := entity.cache[address]
-	if exists {
+func (entity *Mem_cache) Extract(address [32]byte) Instance_cache {
+	val, Exists := entity.Cache[address]
+	if Exists {
 		return val
 	}
 	return Instance_cache{}
 }
 
-func (entity *Mem_cache) exists(key string) bool {
-	address := entity.get_key(key)
+func (entity *Mem_cache) Exists(key string) bool {
+	address := entity.Get_key(key)
 
-	entity.mu.Lock()
-	defer entity.mu.Unlock()
+	entity.Mu.Lock()
+	defer entity.Mu.Unlock()
 
-	_, exists := entity.cache[address]
-	return exists
+	_, Exists := entity.Cache[address]
+	return Exists
 }
